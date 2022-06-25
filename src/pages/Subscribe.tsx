c734b665076f55ffdb9a17aa78356143edb5ad6b
@pedroandradedev <pedro.henrique.ao16@gmail.com>
2022-06-25 17:34:05 -0300

    app

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 import { useCreateSubscriberMutation } from "../graphql/generated";
 
+import codeMockup from '../assets/code-mockup.png'
+
 export function Subscribe() {
   const navigate = useNavigate()
 
@@ -67,7 +69,7 @@ export function Subscribe() {
         </div>
       </div>
 
-      <img src="/src/assets/code-mockup.png" className="mt-10" alt="" />
+      <img src={codeMockup} className="mt-10" alt="" />
     </div>
   )
 }
