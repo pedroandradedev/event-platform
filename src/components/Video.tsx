f380e64b6ab0332b9218d035b59cdbffa3494336
@pedroandradedev <pedro.henrique.ao16@gmail.com>
2022-06-26 05:16:10 -0300

    Responsividade

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -87,33 +87,37 @@ export function Video(props: VideoProps) {
         </div>
 
         <div className="gap-8 mt-20 grid grid-cols-1 lg:grid-cols-2">
-          <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
-            <div className="bg-blue-700 text-gray-700 h-full p-6 flex items-center">
+          <a href="" className="bg-gray-700 rounded overflow-hidden flex flex-col lg:flex-row items-stretch gap-6 hover:bg-gray-600 transition-colors">
+            <div className="bg-blue-700 text-gray-700 h-full p-6 flex items-center justify-center">
               <FileArrowDown size={40} />
             </div>
-            <div className="py-6 leading-relaxed">
-              <strong className="text-2xl">Material complementar</strong>
-              <p className="text-sm- text-gray-200 mt-2">
-                Acesse o material complementar para acelerar o seu desenvolvimento
-              </p>
-            </div>
-            <div className="h-full p-6 flex items-center">
-              <CaretRight size={24} />
+            <div className="flex items-center pl-6 lg:pl-0">
+              <div className="flex-1 py-6 pt-0 lg:pt-6 leading-relaxed">
+                <strong className="text-2xl">Material complementar</strong>
+                <p className="text-sm text-gray-200 mt-2">
+                  Acesse o material complementar para acelerar o seu desenvolvimento
+                </p>
+              </div>
+              <div className="h-full p-6 flex items-center">
+                <CaretRight size={24} />
+              </div>
             </div>
           </a>
 
-          <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+          <a href="" className="bg-gray-700 rounded overflow-hidden flex flex-col lg:flex-row items-stretch gap-6 hover:bg-gray-600 transition-colors">
             <div className="bg-blue-700 text-gray-700 h-full p-6 flex items-center">
               <Image size={40} />
             </div>
-            <div className="py-6 leading-relaxed">
-              <strong className="text-2xl">Wallpapers exclusivos</strong>
-              <p className="text-sm- text-gray-200 mt-2">
+            <div className="flex items-center pl-6 lg:pl-0">
+              <div className="flex-1 py-6 pt-0 lg:pt-6 leading-relaxed">
+                <strong className="text-2xl">Wallpapers exclusivos</strong>
+                <p className="text-sm text-gray-200 mt-2">
                 Baixe wallpapers exclusivos do Ignite Lab e personalize a sua máquina
-              </p>
-            </div>
-            <div className="h-full p-6 flex items-center">
-              <CaretRight size={24} />
+                </p>
+              </div>
+              <div className="h-full p-6 flex items-center">
+                <CaretRight size={24} />
+              </div>
             </div>
           </a>
         </div>
